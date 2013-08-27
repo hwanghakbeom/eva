8473bc2f6d67b14cdc1b456191e6856cb7f89c97
andy <hwanghakbeom@gmail.com>
2013-08-27 17:51:21 +0900

    facebook token

--- app.rb
+++ app.rb
@@ -14,14 +14,26 @@ SITE_URL   = 'http://14.63.164.245/'
 
 SITE_TITLE = "Architect for the World"
 set :bind, '0.0.0.0'
+@myfirstphoto = nil
+
 get '/' do
 	@title = 'EVA'
+	unless session == nil
+	graph = Koala::Facebook::API.new(session['access_token'])
+
+	profile = graph.get_object("me")
+	myuid = profile["id"]
+	session['id'] = myuid
+	sess = session[:id]
+	@myfirstphoto = 'http://res.cloudinary.com/dobny9ati/image/facebook/w_70,h_70,c_fill/'+profile["id"]+'.jpg'
+	end
+
 		  erb :main
 end
 
 get '/callback' do
 		session['access_token'] = session['oauth'].get_access_token(params[:code])
-		redirect '/login'
+		redirect '/'
 end
 
 get '/facebooklogin' do
