b68c8fe7c3dfde271c8cc7fe8c4c62d819c0071c
andy <hwanghakbeom@gmail.com>
2013-09-06 13:32:45 +0900

    add database

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'rubygems'  
 require 'sinatra'  
 require 'koala'
-
+require 'pg'
+require 'data_mapper'
+require 'time'
+require File.join(File.dirname(__FILE__), "urllist.rb")
 
 include Koala
 
@@ -11,6 +14,29 @@ enable :sessions
 APP_ID     = 541378595892777
 APP_SECRET = '10222e30a7db20399a5a10b882b30bcf'
 SITE_URL   = 'http://14.63.164.245/'
+
+url = 'postgres://mpgapjicswidul:Kc53rL4M4jbRm1CgRg5t9onrAr@ec2-54-225-105-169.compute-1.amazonaws.com:5432/d9698rcak6lu5h'
+DataMapper.setup(:default, url)
+
+class Account
+	include DataMapper::Resource
+	property :accountid, Text ,:key =>true
+	property :password, Text
+	property :updated_at, DateTime
+	has n, :users
+	has n, :relations
+	validates_uniqueness_of :accountid, :message => "There's already a registration of that ID"
+end
+
+class User
+	include DataMapper::Resource
+	belongs_to :account
+	property :userid, Text , :key =>true
+	property :name, Text
+	property :sex, Text
+	has n, :photos
+	validates_uniqueness_of :userid, :message => "There's already a registration of that ID"
+end
 
 SITE_TITLE = "Architect for the World"
 set :bind, '0.0.0.0'
@@ -28,6 +54,8 @@ get '/' do
 	@myfirstphoto = 'http://res.cloudinary.com/dobny9ati/image/facebook/w_70,h_70,c_fill/'+profile["id"]+'.jpg'
 	end
 
+	urlclass = Memberurl.new
+	@photoaddress = urlclass.geturl
 	erb :main
 end
 
